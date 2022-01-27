18565c1e1ec9e837365594045a72d9dc0d0da39f
minha619 <minha619@gmail.com>
2022-01-27 00:56:19 -0500

    server/config,mldels,utils added

---
+++ server/models/Wish.js
@@ -0,0 +1,30 @@
+const { Schema, model } = require('mongoose');
+
+const wishSchema = new Schema(
+    {
+        wishText: {
+            type: String,
+            required: 'You need to leave a bucket list wish!',
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            // get: timestamp => dateFormat(timestamp)
+        },
+        username: {
+            type: String,
+            required: true
+        },
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
+
+const Wish = model('Wish', wishSchema);
+
+module.exports = Wish;
