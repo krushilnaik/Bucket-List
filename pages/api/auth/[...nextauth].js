edbd1451e8c24d3f2158c470ebc5f561d6488e75
splaza28 <global-splaza28@gmail.com>
2022-01-29 19:04:04 -0600

    update to nextauth file and added login.js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,12 +1,19 @@
 import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+
+
+export default NextAuth({
+    session: {
+        jwt:true
+    },
+    providers: [
+        Providers.Github({
+            clientId: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET 
+        })
+    ],
+
+})
 
 
 
-
-
-
-
-
-
-
-export default (req, res) => NextAuth(req, res, options);
