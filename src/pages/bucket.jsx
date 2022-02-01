fea0882430d50fcdfdce1ac6fe821035811d97bc
Krushil Naik <krushilnaik96@gmail.com>
2022-02-01 15:24:24 -0500

    Hook todo table to GraphQL

--- src/pages/bucket.jsx
+++ src/pages/bucket.jsx
@@ -2,23 +2,58 @@ import { Group, Tabs, Text } from "@mantine/core";
 import { motion } from "framer-motion";
 import { useState } from "react";
 import { Reorder } from "framer-motion";
-import { useSession } from "next-auth/react";
 import styles from "./styles/Bucket.module.scss";
 import Wish from "../components/Wish";
 import NewWish from "../components/NewWish";
 import Avatar from "../components/Avatar";
+import { getSession, useSession } from "next-auth/react";
+import apolloClient from "../lib/apollo";
+import { QUERY_USER } from "../utils/queries";
+
+export const getServerSideProps = async (ctx) => {
+	const session = await getSession(ctx);
+
+	const { data } = await apolloClient.query({
+		query: QUERY_USER,
+		variables: {
+			userId: session.user.id,
+		},
+	});
+
+	return {
+		props: {
+			wishes: data.user.wishes,
+		},
+	};
+};
 
 const initialTodos = ["🍅 Tomato", "🥒 Cucumber", "🧀 Cheese", "🥬 Lettuce"];
 const initialDones = ["🥑 Avocado", "🍕 Pizza", "🍟 Fries"];
 
-function Bucket() {
-	const [todos, setTodos] = useState(initialTodos);
-	const [dones, setDones] = useState(initialDones);
+function Bucket(props) {
+	const { wishes } = props;
+
+	const [todos, setTodos] = useState(
+		wishes.filter((wish) => !wish.isCompleted)
+	);
+	const [dones, setDones] = useState(wishes.filter((wish) => wish.isCompleted));
 	const { data: session, status } = useSession();
 
 	if (status === "loading") {
 		return <div>loading...</div>;
 	}
+
+	// const { loading, data } = useQuery(QUERY_USER, {
+	// 	variables: {
+	// 		userId: session.user.id,
+	// 	},
+	// });
+
+	// if (loading) {
+	// 	return <div>loading...</div>;
+	// }
+
+	// console.log("the data", data);
 
 	return (
 		<Group direction="row" spacing={30} position="center" align="flex-start">
@@ -51,7 +86,7 @@ function Bucket() {
 							>
 								<NewWish />
 								{todos.map((item) => (
-									<Wish key={item} item={item} />
+									<Wish key={item.id} item={item.wishText} />
 								))}
 							</Reorder.Group>
 						</section>
@@ -65,7 +100,7 @@ function Bucket() {
 								values={dones}
 							>
 								{dones.map((item) => (
-									<Wish key={item} item={item} />
+									<Wish key={item.id} item={item.wishText} />
 								))}
 							</Reorder.Group>
 						</section>
