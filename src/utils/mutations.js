ce9e66593fd867c5c576430c14909bf1a799da45
Krushil Naik <krushilnaik96@gmail.com>
2022-02-02 16:23:24 -0500

    Clean up mutations

--- src/utils/mutations.js
+++ src/utils/mutations.js
@@ -1,28 +1,28 @@
 import { gql } from "@apollo/client";
 
-export const LOGIN_USER = gql`
-	mutation login($email: String!, $password: String!) {
-		login(email: $email, password: $password) {
-			token
-			user {
-				_id
-				name
-			}
-		}
-	}
-`;
+// export const LOGIN_USER = gql`
+// 	mutation login($email: String!, $password: String!) {
+// 		login(email: $email, password: $password) {
+// 			token
+// 			user {
+// 				_id
+// 				name
+// 			}
+// 		}
+// 	}
+// `;
 
-export const ADD_USER = gql`
-	mutation addUser($name: String!, $email: String!, $password: String!) {
-		addUser(name: $name, email: $email, password: $password) {
-			token
-			user {
-				_id
-				name
-			}
-		}
-	}
-`;
+// export const ADD_USER = gql`
+// 	mutation addUser($name: String!, $email: String!, $password: String!) {
+// 		addUser(name: $name, email: $email, password: $password) {
+// 			token
+// 			user {
+// 				_id
+// 				name
+// 			}
+// 		}
+// 	}
+// `;
 
 export const ADD_WISH = gql`
 	mutation addWish($wishText: String!, $userId: String!) {
@@ -32,15 +32,6 @@ export const ADD_WISH = gql`
 		}
 	}
 `;
-
-// export const ADD_WISH = gql`
-// 	mutation addWish($wishText: String!, $userId: String!) {
-// 		addWish(wishText: $wishText, userId: $userId) {
-// 			wishText
-// 			isCompleted
-// 		}
-// 	}
-// `;
 
 export const MARK_AS_DONE = gql`
 	mutation MarkWishDone($wishId: String!) {
